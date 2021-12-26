ca0104e88f25bb1e51536f0e23f42b7a58d4fb22
Rigoberto martinez <donojaci@gmail.com>
2021-12-25 21:26:12 -0600

    le agregamos un metodo especial que nos permite ver informacion sobre el objeto

--- destruir_objeto.py
+++ destruir_objeto.py
@@ -7,10 +7,13 @@ class Pelicula:
         print("se a creado la pelicula {} ".format(self.titulo, ))
         
     #destructor de clase
-    def __del__ (self):
-        print("se esta borrando la pelicula {}".format(self.titulo))
+    #def __del__ (self):
+    #    print("se esta borrando la pelicula {}".format(self.titulo))
     
+    #redefinimos el metodo str 
+    def __str__(self):
+        return "{} lanzada en {} con una duracion de {} minutos".format(self.titulo,self.lanzamiento,self.duracion) 
     
     
 p = Pelicula("El padrino",175,1972)
-del(p)
+print(str(p))
